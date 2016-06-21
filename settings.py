790a28fc284e879a85d1da0c7d2cbcbe2654493b
Dmytro Pershyn <dmitri.pershin@gmail.com>
2016-06-21 22:40:40 +0300

    trd_env working prototype

---
+++ settings.py
@@ -0,0 +1,18 @@
+ENVIRONMENTS = {
+    "streaming": {
+        "real": "stream-fxtrade.oanda.com",
+        "practice": "stream-fxpractice.oanda.com",
+        "sandbox": "stream-sandbox.oanda.com"
+    },
+    "api": {
+        "real": "api-fxtrade.oanda.com",
+        "practice": "api-fxpractice.oanda.com",
+        "sandbox": "api-sandbox.oanda.com"
+    }
+}
+
+DOMAIN = "practice"
+STREAM_DOMAIN = ENVIRONMENTS["streaming"][DOMAIN]
+API_DOMAIN = ENVIRONMENTS["api"][DOMAIN]
+ACCESS_TOKEN = '533cdaec941541162a2011c2b5a607df-531acc53b4cf963b3501db02c040b0a5'
+ACCOUNT_ID = '9084727'
